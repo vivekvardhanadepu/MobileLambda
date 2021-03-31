2a0a97023d2553b8ecc58d1d60cfc79eedf5802f
vivekvardhanadepu <vivekvicky839@gmail.com>
2021-03-31 19:09:01 +0530

    ending worker endeavour(minimum period 15 mins)

--- app/app/src/main/java/com/example/myapplication/fetchRequest.java
+++ app/app/src/main/java/com/example/myapplication/fetchRequest.java
@@ -6,7 +6,9 @@ import androidx.annotation.NonNull;
 import android.content.Context;
 import android.widget.Toast;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintStream;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -22,10 +24,14 @@ import org.json.JSONObject;
 
 import android.util.Log;
 
+import java.io.ByteArrayOutputStream;
+
+import java.nio.charset.StandardCharsets;
+
 public class fetchRequest extends Worker {
 
     private static final String TAG = fetchRequest.class.getSimpleName();
-    private String gateway_url = "http://7f732fdbed45.ngrok.io";
+    private String gateway_url = "https://android-lambda.herokuapp.com";
 
     public fetchRequest(@NonNull Context appContext, @NonNull WorkerParameters workerParams) {
         super(appContext, workerParams);
@@ -65,15 +71,15 @@ public class fetchRequest extends Worker {
                         throw new IOException("Unexpected code " + response);
                     } else {
                         if(response.body().toString().length() > 0 && !response.body().toString().isEmpty()) {
-                            String resp = response.body().string();
+                            String json = response.body().string();
                             JSONObject data = null;
                             try {
-                                data = new JSONObject(resp);
+                                data = new JSONObject(json.substring(json.indexOf("{"), json.lastIndexOf("}") + 1));
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
 
-                            Log.i(TAG, "before Json:" + resp);
+                            Log.i(TAG, "before Json:" + json);
                             Log.i(TAG, "Json String from gateway " + data);
 
                             String code = null;
@@ -88,8 +94,9 @@ public class fetchRequest extends Worker {
                                 e.printStackTrace();
                             }
 
+                            Log.i(TAG, "Running code ");
                             String output = runCode(code);
-
+                            Log.i(TAG, "Sending output ");
                             sendOutput(output, currId, code, input);
                         }
                     }
@@ -110,13 +117,19 @@ public class fetchRequest extends Worker {
         Log.i(TAG, "OnStopped called for this worker");
     }
 
-    public String runCode(String code){
-        Interpreter interpreter = new Interpreter();
+    public String runCode(String code) throws FileNotFoundException {
+        final ByteArrayOutputStream tempStream = new ByteArrayOutputStream();
         String temp = "null";
+        PrintStream out = new PrintStream(tempStream);
+        Interpreter interpreter = new Interpreter();
         try {
-            interpreter.set("context", this);//set any variable, you can refer to it directly from string
+//            System.setOut(out);
+            interpreter.setOut(out);
+            interpreter.set("context", this);   //set any variable,
+                                                        // you can refer to it directly from string
             interpreter.eval(code);//execute code
-            temp = interpreter.get("result").toString();
+            temp = tempStream.toString();
+            Log.i(TAG, "output:" + temp);
         }
         catch (Exception e){    //handle exception
             e.printStackTrace();
@@ -156,7 +169,7 @@ public class fetchRequest extends Worker {
 
                             JSONObject data = null;
                             try {
-                                data = new JSONObject(response.body().toString());
+                                data = new JSONObject(response.body().string());
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
